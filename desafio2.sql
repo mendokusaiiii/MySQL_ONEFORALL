15b226f986242f40af74ed5030f430d3ee12257e
nome completo <email@email.com>
2023-01-19 14:30:03 -0300

    fix: challange 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 SELECT 
     COUNT(DISTINCT mus.musica_id) AS cancoes,
-    COUNT(DISTINCT mus.album_id) AS albuns,
+    COUNT(DISTINCT aa.album_id) AS albuns,
     COUNT(DISTINCT a.artista_id) AS artistas
 
 FROM SpotifyClone.musicas AS mus 
-INNER JOIN album AS alb ON  alb.album_id = mus.album_id
-INNER JOIN artista AS a ON alb.artista_id = a.artista_id;
+INNER JOIN album AS alb ON  alb.album_id = aa.album_id
+INNER JOIN artistas AS a ON alb.artista_id = a.artista_id;
