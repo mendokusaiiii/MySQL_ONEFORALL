d73996d8a0d835f2cacda3c9f49f1568c294753e
nome completo <email@email.com>
2023-01-19 14:47:55 -0300

    feat: challange 3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+SELECT 
+    usuario_nome AS usuario,
+    COUNT(re.musica_id) AS qt_de_musicas_ouvidas,
+    ROUND(SUM(musicas.duracao / 60), 2) AS total_minutos
+FROM SpotifyClone.usuario AS usuario
+INNER JOIN reproducoes as re ON usuario.usuario_id = re.usuario_id
+INNER JOIN musicas as mus ON mus.musica_id = re.musica_id
+GROUP BY usuario_nome 
+ORDER BY usuario_nome;
